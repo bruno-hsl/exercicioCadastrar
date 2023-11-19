1f802f2f7a1bdee96f148ce043669e919568c406
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-11-18 21:23:32 -0300

    Update cadastro.c

--- cadastro.c
+++ cadastro.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #define TAM 2
 
 
@@ -6,45 +7,80 @@ typedef struct{
     char nome[20];
     char email[20];
     char sexo[10];
+    char endereco[50];
+    double altura;
 }usuario;
 
-void cadastrar(usuario u[], int indice){
-    printf("\nDigite o nome do usuario: ");
-    fflush(stdin);
-    fgets(u[indice].nome, sizeof(u[indice].nome), stdin);
-    fflush(stdin);
-    printf("Digite o e-mail do usuario: ");
-    fflush(stdin);
-    fgets(u[indice].email, sizeof(u[indice].email), stdin);
-    fflush(stdin);
-    printf("Digite o sexo do usuario: ");
-    fflush(stdin);
-    fgets(u[indice].sexo, sizeof(u[indice].sexo), stdin);
-    fflush(stdin);
-    
-    do{
-        if(strchr(u[indice].email, '@') == NULL){
+void validarEmail(usuario u[], int indice){
+    while(strchr(u[indice].email, '@') == NULL){
         printf("\nEmail invalido, tente novamente");
         printf("\nDigite o e-mail do usuario: ");
         fflush(stdin);
         fgets(u[indice].email, sizeof(u[indice].email), stdin);
         fflush(stdin);
     }
-    }while(strchr(u[indice].email, '@') == NULL);
-    printf("\n\n");
-
-    do{
-
-    }while();
 }
 
-void buscarRegistro(usuario u[], int totalCadastro, int buscarEmail){
-    if(totalCadastro == 0){
+void validarSexo(usuario u[], int indice){
+    while(!(strcmp(u[indice].sexo, "Masculino") == 0 ||
+            strcmp(u[indice].sexo, "Feminino") == 0 ||
+            strcmp(u[indice].sexo, "Indiferente") == 0)){
+                 printf("Sexo invalido, tente novamente: ");
+                 fflush(stdin);
+                 fgets(u[indice].sexo, sizeof(u[indice].sexo), stdin);
+                 fflush(stdin);
+               }
+}
+
+void validarAltura(usuario u[], int indice){
+    while(u[indice].altura < 1.0 && u[indice].altura > 2.0){
+        printf("Altura invalida, tente novamente");
+        printf("Digite a altura do usuario: ");
+        fflush(stdin);
+        scanf("%lf", &u[indice].altura);
+        fflush(stdin);
+    }
+}
+
+void cadastrar(usuario u[], int indice){
+
+    printf("\nDigite o nome do usuario: ");
+    fflush(stdin);
+    fgets(u[indice].nome, sizeof(u[indice].nome), stdin);
+    fflush(stdin);
+
+    printf("Digite o e-mail do usuario: ");
+    fflush(stdin);
+    fgets(u[indice].email, sizeof(u[indice].email), stdin);
+    fflush(stdin);
+    validarEmail(u, indice);
+
+    printf("Digite o sexo do usuario: ");
+    fflush(stdin);
+    fgets(u[indice].sexo, sizeof(u[indice].sexo), stdin);
+    fflush(stdin);
+    validarSexo(u, indice);
+
+    printf("Digite o endereco do usuario: ");
+    fflush(stdin);
+    fgets(u[indice].endereco, sizeof(u[indice].endereco), stdin);
+    fflush(stdin);
+
+     printf("Digite a altura do usuario: ");
+     fflush(stdin);
+     scanf("%lf", &u[indice].altura);
+     fflush(stdin);
+     validarAltura(u, indice);
+
+}
+
+void buscarRegistro(usuario u[], int totalCadastro, int buscarEmail) {
+    if (totalCadastro == 0) {
         printf("\nNao existe cadastros na lista\n");
         return;
-    }else{
-        for(int i = 0; i < totalCadastro; i++){
-            if(u[i].email == buscarEmail){
+    } else {
+        for (int i = 0; i < totalCadastro; i++) {
+            if (strcmp(u[i].email, buscarEmail) == 0) {
                 printf("\nNome do registro encontrado: %s", u[i].nome);
                 return;
             }
@@ -53,16 +89,16 @@ void buscarRegistro(usuario u[], int totalCadastro, int buscarEmail){
     printf("\nRegistro nao encontrado na lista");
 }
 
-
-
 void imprimir(usuario u[], int totalCadastro){
     if(totalCadastro == 0 ){
         printf("\n\nnao existem usuariaos cadastrados");
     }else{
         for(int i = 0; i < totalCadastro; i++ ){
             printf("\nNome: %s", u[i].nome);
-            printf("\nEmail: %s", u[i].email);
-            printf("\nSexo: %s", u[i].sexo);
+            printf("Email: %s", u[i].email);
+            printf("Sexo: %s", u[i].sexo);
+            printf("\nEndereco: %s", u[i].endereco);
+             printf("Altura: %lf", u[i].altura);
             printf("\n\n");
         }
     }
@@ -78,7 +114,7 @@ int main(){
         printf("\nDigite 3 para buscar um cadastro");
         printf("\nDigite 4 para alterar um cadastro");
         printf("\nDigite 5 para excluir um cadastro");
-        printf("\nDigite 0 para encerrar o programa");
+        printf("\nDigite 0 para encerrar o programa\n");
         scanf("%d", &opcao);
 
         switch(opcao){
